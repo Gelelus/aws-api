4a4def375699077f0aa2d082b53e97098a8dd698
Gelelus <mundor7392290@gmail.com>
2020-11-04 13:09:51 +0300

    global formating

--- product-service/main.ts
+++ product-service/main.ts
@@ -1,6 +1,6 @@
-import 'source-map-support/register';
+import "source-map-support/register";
 
-import {getAllProducts} from './src/requestHandlers/getAll';
-import {getProductById} from './src/requestHandlers/getByID';
+import { getAllProducts } from "./src/requestHandlers/getAll";
+import { getProductById } from "./src/requestHandlers/getByID";
 
-export {getAllProducts, getProductById}
+export { getAllProducts, getProductById };
--- product-service/src/requestHandlers/getAll.ts
+++ product-service/src/requestHandlers/getAll.ts
@@ -1,12 +1,12 @@
-import {APIGatewayProxyHandler} from "aws-lambda";
+import { APIGatewayProxyHandler } from "aws-lambda";
 
-import products from '../data/products';
-import { getHeaders } from '../helpers/index'
+import products from "../data/products";
+import { getHeaders } from "../helpers/index";
 
-export const getAllProducts : APIGatewayProxyHandler = async ()  => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify(products),
-        headers: getHeaders(),
-    };
+export const getAllProducts: APIGatewayProxyHandler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(products),
+    headers: getHeaders(),
+  };
 };
